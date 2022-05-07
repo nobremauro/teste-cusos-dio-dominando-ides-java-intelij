6c85b6e9113cc21bebd24eb2c679e5d7878abd88
Mauro Nobre <mauro.s.nobre@gmail.com>
2022-05-07 18:24:08 -0300

    "Continuaçao em casa"

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,16 +4,16 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        int a = 5;
+        /*int a = 5;
         int b = 3;
-        System.out.println("Hello World! " + (a + b));
+        System.out.println("Hello World! " + (a + b));*/
 
-        Gato gato = new Gato();
+        Gato gato = new Gato("Tang", "azul", 10);
+
         System.out.println(gato);
 
-        Livro livro1 = new Livro(nome   "O Problema dos 3 Corpos", numPaginas 300);
+        Livro livro1 = new Livro("O Problema dos 3 Corpos",  300);
         System.out.println(livro1);
-
 
     }
 
@@ -22,4 +22,32 @@ class Livro {
     private String nome;
     private Integer numPaginas;
 
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
--- src/br/com/dio/model/Gato.java
+++ src/br/com/dio/model/Gato.java
@@ -1,5 +1,7 @@
 package br.com.dio.model;
 
+import java.util.Objects;
+
 public class Gato {
     private String nome;
     private String cor;
@@ -9,6 +11,46 @@ public class Gato {
         this.nome = nome;
         this.cor = cor;
         this.idade = idade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
     }
 }
 
